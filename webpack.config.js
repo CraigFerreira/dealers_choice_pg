b4366bbedcd60b63a09d05b70d9613f801735d55
Craig Ferreira <ferreira.craig@gmail.com>
2021-03-01 14:33:56 -0500

    updated

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: [
+      './src/'
+    ],
+    module: {
+      rules: [
+        {
+          test:/\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
+            }
+          }
+        }
+      ]
+    }
+  };
